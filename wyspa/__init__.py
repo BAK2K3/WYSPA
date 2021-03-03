b6f081e15ac9bd2af378195baae79d5b53b56a90
BAK2K3 <benjamin.a.kavanagh@gmail.com>
2021-03-03 18:19:19 +0000

    Initialise Flask from main __init__ file.

---
+++ wyspa/__init__.py
@@ -0,0 +1,25 @@
+"""
+WYSPA - A Full Stack Anonymous Social Media Network
+===================================================
+
+WYSPA is a Full Stack Project utilising Python, NoSQL,
+JavaScript, HTML, and CSS. It aims to give voices
+to those who want to speak their mind, anonymously.
+
+See https://github.com/BAK2K3/WYSPA/blob/master/README.md
+for complete documentation.
+
+"""
+
+import os
+from flask import Flask
+if os.path.exists("env.py"):
+    import env
+
+app = Flask(__name__)
+app.secret_key = os.environ.get("SECRET_KEY")
+
+
+@app.route("/")
+def index():
+    return "Hello, World!"
