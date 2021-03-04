360b82ee41c10da7dacbe13fbcb8f26537aa4bc1
BAK2K3 <benjamin.a.kavanagh@gmail.com>
2021-03-04 17:04:31 +0000

    Add Application Factory to avoid circular imports.

---
+++ wyspa/factory/initialisation.py
@@ -0,0 +1,25 @@
+"""
+Initialiase Flask Application Factory with MongoDB.
+"""
+import os
+from flask import Flask
+from flask_pymongo import PyMongo
+if os.path.exists("env.py"):
+    import env
+
+# https://stackoverflow.com/questions/48653120/flask-pymongo-with-application-factory-and-blueprints
+
+mongo = PyMongo()
+
+
+def create_app():
+    app = Flask(__name__, instance_relative_config=False,
+                template_folder=os.path.abspath('wyspa/templates'))
+
+    app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+    app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+    app.secret_key = os.environ.get("SECRET_KEY")
+
+    mongo.init_app(app)
+
+    return app
