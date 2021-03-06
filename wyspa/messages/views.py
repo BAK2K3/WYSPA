df34206c26d814d000fd28555bc3bd7adcaf2bc0
BAK2K3 <benjamin.a.kavanagh@gmail.com>
2021-03-06 19:17:09 +0000

    Update function to serve single message.

--- wyspa/messages/views.py
+++ wyspa/messages/views.py
@@ -11,4 +11,4 @@ def view_message():
 
     messages_dict = [{"Name": "Ben", "Location": "Northampton"},
                      {"Name": "Deiza", "Location": "Northampton"}]
-    return render_template('messages.html', messages_dict=messages_dict)
+    return render_template('messages.html', messages_dict=messages_dict[0])
