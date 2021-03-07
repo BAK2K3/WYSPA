e0be82ed01b5d1baa3f3ee9ddd1c5ba8511c9672
BAK2K3 <benjamin.a.kavanagh@gmail.com>
2021-03-07 19:25:37 +0000

    Add init for collapsible.

--- wyspa/static/js/init.js
+++ wyspa/static/js/init.js
@@ -1,4 +1,11 @@
+// Sidenav init
 document.addEventListener('DOMContentLoaded', function () {
     var elems = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(elems, {});
+});
+
+// Collapsable Init
+document.addEventListener('DOMContentLoaded', function () {
+    var elems = document.querySelectorAll('.collapsible');
+    var instances = M.Collapsible.init(elems, {});
 });
