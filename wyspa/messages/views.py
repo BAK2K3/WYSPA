9e76de44743b3e25cca8c6d10cd07baa872f24ac
BAK2K3 <benjamin.a.kavanagh@gmail.com>
2021-03-11 18:54:57 +0000

    Re-add db query for layout testing.

--- wyspa/messages/views.py
+++ wyspa/messages/views.py
@@ -9,7 +9,6 @@ messages = Blueprint('messages', __name__)
 @messages.route('/view_message')
 def view_message():
 
-    messages_dict = [{"Name": "Ben", "Location": "Northampton",
-                      "Message": "Hello, World!"},
-                     {"Name": "Deiza", "Location": "Northampton", "Message": "This message is going to be a little bit longer. It's going to try and make the box expand to it's fullest."}]
-    return render_template('messages.html', messages_dict=messages_dict[0])
+    message_entry = mongo.db.messages.find()
+
+    return render_template('messages.html', message_entry=message_entry)
