5396bca960d80f3a569ac0e70c34d5120626605e
BAK2K3 <benjamin.a.kavanagh@gmail.com>
2021-03-13 19:47:06 +0000

    Add Login Manager and user view.

--- wyspa/__init__.py
+++ wyspa/__init__.py
@@ -15,9 +15,16 @@ from wyspa.core.views import core
 from wyspa.maps.views import maps
 from wyspa.messages.views import messages
 from wyspa.factory.initialisation import create_app
+from wyspa.users.views import login_manager, users
 
 
 app = create_app()
 app.register_blueprint(core)
 app.register_blueprint(maps)
 app.register_blueprint(messages)
+app.register_blueprint(users)
+
+# Set up log in manager
+login_manager.init_app(app)
+login_manager.session_protection = "strong"
+login_manager.login_view = 'core.index'
