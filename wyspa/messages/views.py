b3ee5939400dec3b18ca903aa537a224cd07bc2a
BAK2K3 <benjamin.a.kavanagh@gmail.com>
2021-03-14 10:52:18 +0000

    Add login-required for my_voice.

--- wyspa/messages/views.py
+++ wyspa/messages/views.py
@@ -1,5 +1,5 @@
 from flask import render_template, Blueprint
-from flask_login import current_user
+from flask_login import current_user, login_required
 from wyspa.factory.initialisation import mongo
 
 
@@ -17,6 +17,7 @@ def view_message():
 
 
 @messages.route('/my_voice')
+@login_required
 def my_voice():
 
     users_messages = list(mongo.db.messages.find(
