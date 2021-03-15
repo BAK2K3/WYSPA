22546a1d263b38c415cd70ed7ee1b9bfa4c953c5
BAK2K3 <benjamin.a.kavanagh@gmail.com>
2021-03-15 18:24:13 +0000

    Move WYSPA class to seperate module.

---
+++ wyspa/messages/classes.py
@@ -0,0 +1,49 @@
+from bson.objectid import ObjectId
+from wyspa.factory.initialisation import mongo
+
+
+# Create a class for WYSPAs
+class Wyspa():
+    def __init__(self, author, message, mood, location, comments=[], _id=None):
+        self._id = _id
+        self.author = author
+        self.message = message
+        self.mood = mood
+        self.location = location
+        self.comments = comments if comments else []
+
+    def get_info(self):
+        # Return Dictionary for DB
+        info = {'author': self.author, 'message': self.message,
+                'mood': self.mood, 'location': self.location,
+                'comments': self.comments}
+        return info
+
+    def remove_comment(self, index):
+        self.comments.pop(index)
+        mongo.db.messages.update({"_id": ObjectId(self._id)}, self.get_info())
+
+    def add_comment(self, new_comment):
+        self.comments.append({self.author: new_comment})
+        mongo.db.messages.update({"_id": ObjectId(self._id)}, self.get_info())
+
+    def delete_wyspa(self):
+        mongo.db.messages.remove({"_id": ObjectId(self._id)})
+
+    def write_wyspa(self):
+        mongo.db.messages.insert_one(self.get_info())
+
+    @ classmethod
+    def get_by_id(cls, _id):
+        data = mongo.db.messages.find_one({"_id": ObjectId(_id)})
+        if data is not None:
+            return cls(**data)
+
+    @ classmethod
+    def get_by_user(cls, username):
+        data = list(mongo.db.messages.find({"author": username}))
+        if data is not None:
+            return_data = []
+            for message in data:
+                return_data.append(cls(**message))
+            return return_data
