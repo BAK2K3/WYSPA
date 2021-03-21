0e760378c3d3464c7e48a943ce65f36c426d7b94
BAK2K3 <benjamin.a.kavanagh@gmail.com>
2021-03-21 15:15:30 +0000

    Add marker functionality to map view.

--- wyspa/maps/views.py
+++ wyspa/maps/views.py
@@ -1,5 +1,5 @@
 from flask import render_template, Blueprint
-from wyspa.factory.initialisation import mongo
+from wyspa.messages.classes import Wyspa
 import os
 
 GMAPS_API = os.environ.get("GMAPS_API")
@@ -11,4 +11,12 @@ maps = Blueprint('maps', __name__)
 @maps.route('/map_overview')
 def map_overview():
 
-    return render_template('maps.html', GMAPS_API=GMAPS_API)
+    # Retrieve all messages
+    all_messages = Wyspa.get_all_wyspas()
+
+    # Convert data from messages to id and latlng
+    map_data = Wyspa.wyspa_to_map(all_messages)
+
+    return render_template('maps.html',
+                           GMAPS_API=GMAPS_API,
+                           map_data=map_data)
