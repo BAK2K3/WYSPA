bc45058555fa0df87e580db16f7842199d2f4b2e
BAK2K3 <benjamin.a.kavanagh@gmail.com>
2021-03-21 15:21:11 +0000

    Add latlng conversion to wyspa creation.

--- wyspa/messages/views.py
+++ wyspa/messages/views.py
@@ -8,10 +8,12 @@ from wyspa.maps.locations import location_to_latlong
 messages = Blueprint('messages', __name__)
 
 
+# Route for random messages or specific messages
 @ messages.route('/view_message', defaults={'message_id': None})
 @ messages.route('/view_message/<message_id>')
 def view_message(message_id):
 
+    # Return random message unless specified
     if message_id:
         message_entry = Wyspa.get_by_id(message_id)
     else:
@@ -20,6 +22,7 @@ def view_message(message_id):
     return render_template('messages.html', message_entry=message_entry)
 
 
+# "profile" page for viewing all wyspas and creating wyspas
 @ messages.route('/my_voice')
 @ login_required
 def my_voice():
@@ -28,10 +31,12 @@ def my_voice():
     return render_template('my_voice.html', users_messages=users_messages)
 
 
+# Wyspa creation
 @ messages.route('/create_wyspa', methods=["GET", "POST"])
 @ login_required
 def create_wyspa():
 
+    # Try and convert the address to latlong
     try:
         location_to_latlong(request.form.get("postcode"))
     except Exception as e:
@@ -39,6 +44,7 @@ def create_wyspa():
         flash("Unable to locate address!")
         return redirect(url_for("messages.my_voice"))
 
+    # Create a new wyspa, and write to DB
     if request.method == "POST":
         new_wyspa = Wyspa(current_user.username,
                           request.form.get("wyspaContent"),
@@ -48,6 +54,7 @@ def create_wyspa():
         return redirect(url_for("messages.my_voice"))
 
 
+# Add comment to existing Wyspa
 @ messages.route('/add_comment/<message_id>', methods=["GET", "POST"])
 def add_comment(message_id):
 
@@ -60,7 +67,9 @@ def add_comment(message_id):
                                 message_id=retrieved_wyspa._id))
 
 
+# Remove Wyspa
 @ messages.route('/remove_wyspa/<message_id>', methods=["GET", "POST"])
+@ login_required
 def remove_wyspa(message_id):
 
     if request.method == "POST":
@@ -68,12 +77,16 @@ def remove_wyspa(message_id):
         return redirect(url_for("messages.my_voice"))
 
 
+# Control center for My Voice hub
 @ messages.route('/message_control/<message_id>', methods=["GET", "POST"])
 def message_control(message_id):
     if request.method == "POST":
+        # Navigation for deletion
         if "delete" in request.form:
             Wyspa.delete_wyspa(message_id)
             return redirect(url_for("messages.my_voice"))
+
+        # Navigation for viewing message
         elif "goto" in request.form:
             return redirect(url_for("messages.view_message",
                                     message_id=message_id))
