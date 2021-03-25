a95ced8dfb16c34220569dc69f8d48d122b48d9e
BAK2K3 <benjamin.a.kavanagh@gmail.com>
2021-03-25 08:45:39 +0000

    Remove redundant code from core routes.

--- wyspa/core/views.py
+++ wyspa/core/views.py
@@ -1,14 +1,11 @@
 from flask import render_template, Blueprint
-from wyspa.factory.initialisation import mongo
 
 
 # Configure Blueprint for core route
 core = Blueprint('core', __name__)
 
 
+# Route for home page
 @core.route('/')
 def index():
-
-    messages = [{"Name": "Ben", "Location": "Northampton"},
-                {"Name": "Deiza", "Location": "Northampton"}]
-    return render_template('index.html', messages=messages)
+    return render_template('index.html')
