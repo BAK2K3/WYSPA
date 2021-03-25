c4db33bcdfb5bf98869b3e598701bcb1e8fa0aee
BAK2K3 <benjamin.a.kavanagh@gmail.com>
2021-03-25 08:49:01 +0000

    Import and register error handling routes.

--- wyspa/__init__.py
+++ wyspa/__init__.py
@@ -14,6 +14,7 @@ for complete documentation.
 from wyspa.core.views import core
 from wyspa.maps.views import maps
 from wyspa.messages.views import messages
+from wyspa.error_pages.handlers import error_pages
 from wyspa.factory.initialisation import create_app
 from wyspa.users.views import login_manager, users
 
@@ -23,6 +24,7 @@ app.register_blueprint(core)
 app.register_blueprint(maps)
 app.register_blueprint(messages)
 app.register_blueprint(users)
+app.register_blueprint(error_pages)
 
 # Set up log in manager
 login_manager.init_app(app)
