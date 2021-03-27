69d6cd5acbe3eca1311fa445e148b8af19e8560d
BAK2K3 <benjamin.a.kavanagh@gmail.com>
2021-03-27 11:42:11 +0000

    Add default value for radius.

--- wyspa/static/js/maps.js
+++ wyspa/static/js/maps.js
@@ -45,7 +45,7 @@ function initMap(map_data) {
             fillColor: wyspaColor,
             fillOpacity: 0.35,
             map,
-            radius: map_data[message]["listens"] * 10000,
+            radius: 10000 + (map_data[message]["listens"] * 10000),
             center: map_data[message]["location"],
             message_id: map_data[message]["_id"],
         });
