b1091771e4d579a37a6e4c7f0c00b63af6f51e0a
BAK2K3 <benjamin.a.kavanagh@gmail.com>
2021-03-28 09:05:00 +0000

    Add location scrambling.

--- wyspa/maps/locations.py
+++ wyspa/maps/locations.py
@@ -1,9 +1,10 @@
 from geopy.geocoders import Nominatim
+from random import random
 
 
 def location_to_latlong(user_location):
     geolocator = Nominatim(user_agent="WYSPA")
     location = geolocator.geocode(user_location)
-    latlong = {"lat": location.latitude,
-               "lng": location.longitude}
+    latlong = {"lat": location.latitude + (round(random(), 10) / 10),
+               "lng": location.longitude + (round(random(), 10) / 10)}
     return latlong
