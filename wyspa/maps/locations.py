8a0d7eb600b0d0c6584dfaf414d389a56f6af368
BAK2K3 <benjamin.a.kavanagh@gmail.com>
2021-03-29 15:50:26 +0000

    Update lat/long scrambling for +-0.05 adjustment.

--- wyspa/maps/locations.py
+++ wyspa/maps/locations.py
@@ -5,6 +5,6 @@ from random import random
 def location_to_latlong(user_location):
     geolocator = Nominatim(user_agent="WYSPA")
     location = geolocator.geocode(user_location)
-    latlong = {"lat": location.latitude + (round(random(), 10) / 10),
-               "lng": location.longitude + (round(random(), 10) / 10)}
+    latlong = {"lat": location.latitude + (round((random() / 10), 10)-0.05),
+               "lng": location.longitude + (round((random() / 10), 10)-0.05)}
     return latlong
