24fec4a744151f9de35069580421ad521ff1ed61
BAK2K3 <benjamin.a.kavanagh@gmail.com>
2021-03-29 17:38:38 +0000

    Change random() to uniform() for  +-0.01.

--- wyspa/maps/locations.py
+++ wyspa/maps/locations.py
@@ -1,10 +1,10 @@
 from geopy.geocoders import Nominatim
-from random import random
+from random import uniform
 
 
 def location_to_latlong(user_location):
     geolocator = Nominatim(user_agent="WYSPA")
     location = geolocator.geocode(user_location)
-    latlong = {"lat": location.latitude + (round((random() / 10), 10)-0.05),
-               "lng": location.longitude + (round((random() / 10), 10)-0.05)}
+    latlong = {"lat": location.latitude + (round(uniform(0.1, -0.1), 10)),
+               "lng": location.longitude + (round(uniform(0.1, -0.1), 10))}
     return latlong
