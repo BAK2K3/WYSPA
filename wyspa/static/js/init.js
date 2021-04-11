a13cf78fa11133c6c96f409c44787a4b3d67c706
BAK2K3 <benjamin.a.kavanagh@gmail.com>
2021-04-11 10:29:20 +0000

    Add targetted tooltip init and reformat code.

--- wyspa/static/js/init.js
+++ wyspa/static/js/init.js
@@ -1,48 +1,69 @@
 // Sidenav init
-document.addEventListener('DOMContentLoaded', function () {
-    var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems, {});
+document.addEventListener("DOMContentLoaded", function () {
+  var elems = document.querySelectorAll(".sidenav");
+  var instances = M.Sidenav.init(elems, {});
 });
 
 // Collapsable Init
-document.addEventListener('DOMContentLoaded', function () {
-    var elems = document.querySelectorAll('.collapsible');
-    var instances = M.Collapsible.init(elems, {});
+document.addEventListener("DOMContentLoaded", function () {
+  var elems = document.querySelectorAll(".collapsible");
+  var instances = M.Collapsible.init(elems, {});
 });
 
 // Modal Init
-document.addEventListener('DOMContentLoaded', function () {
-    var elems = document.querySelectorAll('.modal');
-    var instances = M.Modal.init(elems, options = {
-        startingTop: '10%',
-        endingTop: '10%'
-    });
+document.addEventListener("DOMContentLoaded", function () {
+  var elems = document.querySelectorAll(".modal");
+  var instances = M.Modal.init(
+    elems,
+    (options = {
+      startingTop: "10%",
+      endingTop: "10%",
+    })
+  );
 });
 
 // Tooltip init
-document.addEventListener('DOMContentLoaded', function () {
-    var elems = document.querySelectorAll('.tooltipped');
-    var instances = M.Tooltip.init(elems, option = {
-        enterDelay: 100,
-        outDuration: 50
-    });
+document.addEventListener("DOMContentLoaded", function () {
+  var elems = document.querySelectorAll(".tooltipped.wyspa-action-icon");
+  var instances = M.Tooltip.init(
+    elems,
+    (option = {
+      margin: 0,
+      enterDelay: 100,
+      outDuration: 50,
+    })
+  );
+  var elems = document.querySelectorAll(".tooltipped:not(.wyspa-action-icon)");
+  var instances = M.Tooltip.init(
+    elems,
+    (option = {
+      enterDelay: 100,
+      outDuration: 50,
+    })
+  );
 });
 
 // Date picker init
-document.addEventListener('DOMContentLoaded', function () {
-    var elems = document.querySelectorAll('.datepicker');
-    var instances = M.Datepicker.init(elems, options = {
-        minDate: new Date(),
-        format: 'dd-mm-yyyy',
-        showClearBtn: true
-    });
+document.addEventListener("DOMContentLoaded", function () {
+  var elems = document.querySelectorAll(".datepicker");
+  var instances = M.Datepicker.init(
+    elems,
+    (options = {
+      minDate: new Date(),
+      format: "dd-mm-yyyy",
+      showClearBtn: true,
+    })
+  );
 });
 
 // Time Picker init
-document.addEventListener('DOMContentLoaded', function () {
-    var elems = document.querySelectorAll('.timepicker');
-    var instances = M.Timepicker.init(elems, options = {
-        twelveHour: false,
-        showClearBtn: true
-    });
+document.addEventListener("DOMContentLoaded", function () {
+  var elems = document.querySelectorAll(".timepicker");
+  var instances = M.Timepicker.init(
+    elems,
+    (options = {
+      twelveHour: false,
+      showClearBtn: true,
+    })
+  );
 });
