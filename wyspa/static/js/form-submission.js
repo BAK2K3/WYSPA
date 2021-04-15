1c5582968388bd7051ec2bc8b2b7466eb4e69549
BAK2K3 <benjamin.a.kavanagh@gmail.com>
2021-04-15 07:27:22 +0000

    Add disabledInput function for date/time pickers.

--- wyspa/static/js/form-submission.js
+++ wyspa/static/js/form-submission.js
@@ -11,3 +11,18 @@ document.querySelectorAll("form").forEach((form) => {
     form.classList.add("is-submitting");
   });
 });
+
+// https://stackoverflow.com/questions/12777751/html-required-readonly-input-in-form
+// Prevent user from typing in Expiry Date/Time, with it still be required input
+
+const disableInput = function (event) {
+  // ignore tab
+  if (event.keyCode != 9) {
+    event.preventDefault();
+  }
+};
+
+document.querySelectorAll(".date-field").forEach((field) => {
+  field.addEventListener("keydown", disableInput, false);
+  field.addEventListener("paste", disableInput, false);
+});
