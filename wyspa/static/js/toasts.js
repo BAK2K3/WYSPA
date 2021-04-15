3818dfb909dabff3e52e3d101d4fed9be5de7ddf
BAK2K3 <benjamin.a.kavanagh@gmail.com>
2021-04-15 16:03:57 +0000

    Update html call (unsafehtml) to prevent warning.

--- wyspa/static/js/toasts.js
+++ wyspa/static/js/toasts.js
@@ -1,10 +1,10 @@
 function displayToast(text) {
-    var toastHTML = `${text} <button class="btn-flat toast-action" onclick="dismissToast()">Dismiss</button>`;
-    M.toast({ html: toastHTML, classes: 'styled-toast center' });
+  var toastHTML = `${text} <button class="btn-flat toast-action" onclick="dismissToast()">Dismiss</button>`;
+  M.toast({ unsafeHTML: toastHTML, classes: "styled-toast center" });
 }
 
 function dismissToast() {
-    var toastElement = document.querySelector('.toast');
-    var toastInstance = M.Toast.getInstance(toastElement);
-    toastInstance.dismiss();
+  var toastElement = document.querySelector(".toast");
+  var toastInstance = M.Toast.getInstance(toastElement);
+  toastInstance.dismiss();
 }
