4711c1d692d00281bc6ef639a9a0a41f8a8c1591
BAK2K3 <benjamin.a.kavanagh@gmail.com>
2021-04-15 11:52:07 +0000

    Rename slider.js to wyspa-form.js.!

---
+++ wyspa/static/js/wyspa-form.js
@@ -0,0 +1,49 @@
+// Change the slider label to corresponding mood based on value, and add respective class
+const rangeToMood = {
+  1: ["Sad", "negativeLabel"],
+  2: ["Neutral", "neutralLabel"],
+  3: ["Happy", "positiveLabel"],
+};
+
+const rangeSlider = document.querySelector("input#mood");
+const thumbValue = document.querySelector(".range-field span.thumb span.value");
+const rangeLabel = document.querySelector("label[for='mood'] span");
+
+rangeSlider.addEventListener("change", function () {
+  rangeLabel.className = rangeToMood[thumbValue.innerText][1];
+  rangeLabel.innerHTML = rangeToMood[thumbValue.innerText][0];
+});
+
+window.onload = function () {
+  thumbValue.innerHTML = rangeSlider.value;
+  rangeSlider.dispatchEvent(new Event("change"));
+};
+
+// https://stackoverflow.com/questions/12777751/html-required-readonly-input-in-form
+// Prevent user from typing in Expiry Date/Time, with it still be required input
+
+const disableInput = function (event) {
+  // ignore tab
+  if (event.keyCode != 9) {
+    event.preventDefault();
+  }
+};
+
+document.querySelectorAll(".date-field").forEach((field) => {
+  field.addEventListener("keydown", disableInput, false);
+  field.addEventListener("paste", disableInput, false);
+});
+
+// Script for changing the header text of the "create a wyspa" Collapsable
+const createHeader = document.querySelector(".create-header");
+
+createHeader.addEventListener("click", () => {
+  if (createHeader.classList.contains("activated")) {
+    createHeader.classList.remove("activated");
+    createHeader.innerHTML =
+      "Create a WYSPA <i class='material-icons right'>chat_bubble_outline</i>";
+  } else {
+    createHeader.classList.add("activated");
+    createHeader.innerHTML = "The world is listening...";
+  }
+});
