4b40c69b546ab4417d42b7e33a8d3a3528a726fd
BAK2K3 <benjamin.a.kavanagh@gmail.com>
2021-04-15 07:54:26 +0000

    Update formatting of code.

--- wyspa/static/js/slider.js
+++ wyspa/static/js/slider.js
@@ -1,8 +1,7 @@
-const rangeToMood =
-{
-    "1": ["Sad", "negativeLabel"],
-    "2": ["Neutral", "neutralLabel"],
-    "3": ["Happy", "positiveLabel"]
+const rangeToMood = {
+  1: ["Sad", "negativeLabel"],
+  2: ["Neutral", "neutralLabel"],
+  3: ["Happy", "positiveLabel"],
 };
 
 const rangeSlider = document.querySelector("input#mood");
@@ -10,13 +9,11 @@ const thumbValue = document.querySelector(".range-field span.thumb span.value");
 const rangeLabel = document.querySelector("label[for='mood'] span");
 
 rangeSlider.addEventListener("change", function () {
-
-    rangeLabel.className = rangeToMood[thumbValue.innerText][1];
-    rangeLabel.innerHTML = rangeToMood[thumbValue.innerText][0];
-
+  rangeLabel.className = rangeToMood[thumbValue.innerText][1];
+  rangeLabel.innerHTML = rangeToMood[thumbValue.innerText][0];
 });
 
 window.onload = function () {
-    thumbValue.innerHTML = rangeSlider.value;
-    rangeSlider.dispatchEvent(new Event("change"));
+  thumbValue.innerHTML = rangeSlider.value;
+  rangeSlider.dispatchEvent(new Event("change"));
 };
