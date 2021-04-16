631ca964243ec127b481d1d4666a0b44f0c86316
BAK2K3 <benjamin.a.kavanagh@gmail.com>
2021-04-16 10:01:33 +0000

    Remove reg form from double submission script.

--- wyspa/static/js/form-submission.js
+++ wyspa/static/js/form-submission.js
@@ -1,13 +1,12 @@
 // https://www.bram.us/2020/11/04/preventing-double-form-submissions/
 // Prevent Double Submits on Form Submission
-document.querySelectorAll("form").forEach((form) => {
+document.querySelectorAll("form:not(.user-registration)").forEach((form) => {
   form.addEventListener("submit", (e) => {
     // Prevent if already submitting
     if (form.classList.contains("is-submitting")) {
       e.preventDefault();
     }
-
-    // Add class to hook our visual indicator on
+    // Add class to hook our disabler
     form.classList.add("is-submitting");
   });
 });
