0eb5680b5dc232c174fd850f1d9a7934ca36d8e0
BAK2K3 <benjamin.a.kavanagh@gmail.com>
2021-04-16 12:29:57 +0000

    Reduce toast time by half.

--- wyspa/static/js/toasts.js
+++ wyspa/static/js/toasts.js
@@ -1,6 +1,10 @@
 function displayToast(text) {
   var toastHTML = `${text} <button class="btn-flat toast-action" onclick="dismissToast()">Dismiss</button>`;
-  M.toast({ unsafeHTML: toastHTML, classes: "styled-toast center" });
+  M.toast({
+    unsafeHTML: toastHTML,
+    classes: "styled-toast center",
+    displayLength: 2000,
+  });
 }
 
 function dismissToast() {
