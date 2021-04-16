4487e2d233cf2e1d16a048ba3e3d29314db5713e
BAK2K3 <benjamin.a.kavanagh@gmail.com>
2021-04-16 10:11:31 +0000

    Consolidate validate and form-submission scripts.

--- wyspa/static/js/validate.js
+++ wyspa/static/js/validate.js
@@ -80,3 +80,16 @@ registrationForm.addEventListener("submit", function (event) {
     registrationForm.classList.add("is-submitting");
   }
 });
+
+// https://www.bram.us/2020/11/04/preventing-double-form-submissions/
+// Prevent Double Submits on Form Submission
+document.querySelectorAll("form:not(.user-registration)").forEach((form) => {
+  form.addEventListener("submit", (e) => {
+    // Prevent if already submitting
+    if (form.classList.contains("is-submitting")) {
+      e.preventDefault();
+    }
+    // Add class to hook our disabler
+    form.classList.add("is-submitting");
+  });
+});
