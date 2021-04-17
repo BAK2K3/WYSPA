50d06c170dc325d36ebe68b252eb44c07d41e257
BAK2K3 <benjamin.a.kavanagh@gmail.com>
2021-04-17 16:09:57 +0000

    Fix typo in docstring.

--- wyspa/core/__init__.py
+++ wyspa/core/__init__.py
@@ -2,7 +2,7 @@
 Core Module
 =============
 
-This module contains the core Views submodules.
+This module contains the core Views sub-modules.
 
 Sub-Module:
     - Views (Flask Routes for core functionality)
