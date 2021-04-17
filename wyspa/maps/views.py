6a762eaa7b02b9dceb3ae83a8df16476278c1842
BAK2K3 <benjamin.a.kavanagh@gmail.com>
2021-04-17 17:39:27 +0000

    Add docstrings to maps/views.py.

--- wyspa/maps/views.py
+++ wyspa/maps/views.py
@@ -1,10 +1,22 @@
+"""
+Views - Maps Sub-module
+==================================
+
+This module contains the Maps Views sub-module which
+handles data preparation and routing for the Map
+feature.
+
+Functions:
+    map_overview()
+"""
+
 import os
 
 from flask import render_template, Blueprint
 
 from wyspa.messages.classes import Wyspa
 
-
+# Set the Google Maps API Key to be passed to view
 GMAPS_API = os.environ.get("GMAPS_API")
 
 # Configure Blueprint for maps route
@@ -13,6 +25,12 @@ maps = Blueprint('maps', __name__)
 
 @maps.route('/map_overview')
 def map_overview():
+    """Map Route
+
+    This route queries the DB for all Wyspas,
+    prepares the data, and sends both the Gmaps API and map_data
+    to the rendered HTML page.
+    """
 
     # Retrieve all messages
     all_messages = Wyspa.get_all_wyspas()
@@ -20,6 +38,7 @@ def map_overview():
     # Convert data from messages to id and latlng
     map_data = Wyspa.wyspa_to_map(all_messages)
 
+    # Render Map template, passing in API and map data
     return render_template('maps.html',
                            GMAPS_API=GMAPS_API,
                            map_data=map_data)
