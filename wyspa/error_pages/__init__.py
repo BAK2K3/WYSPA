96ff8ae0bf3ef3a882ea2d7ff70b58f050d10a78
BAK2K3 <benjamin.a.kavanagh@gmail.com>
2021-04-17 15:52:12 +0000

    Fix typo in docstring.

--- wyspa/error_pages/__init__.py
+++ wyspa/error_pages/__init__.py
@@ -3,7 +3,7 @@ Error_Pages Module
 =============
 
 This module contains the Error Handling
-sub-module for Flask Routing
+sub-module for Flask Routing.
 
 Sub-Module:
     - Handlers (Flask Routes for Error Pages)
