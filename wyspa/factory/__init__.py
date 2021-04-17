7b1fb5bd81e83411e47d1f584a9eef9033816ec8
BAK2K3 <benjamin.a.kavanagh@gmail.com>
2021-04-17 15:56:59 +0000

    Remove newline from docstring.

---
+++ wyspa/factory/__init__.py
@@ -0,0 +1,10 @@
+"""
+Factory Module
+=============
+
+This module contains the Application Factory
+sub-module for Flask Initialisation.
+
+Sub-Module:
+    - Initialisation (Flask, MongoDB, and Prettify Setup)
+"""
