6fa3af9eb3d06366798c07519fe685e018a3aa98
BAK2K3 <benjamin.a.kavanagh@gmail.com>
2021-04-17 15:50:06 +0000

    Add docstrings and comments.

--- wyspa/error_pages/handlers.py
+++ wyspa/error_pages/handlers.py
@@ -1,3 +1,13 @@
+"""
+Handlers - Error_Pages Submodule
+=============
+This submodule is for handling error pages.
+
+Functions:
+    - Error_404(error)
+
+"""
+
 from flask import render_template, Blueprint
 
 
@@ -8,4 +18,8 @@ error_pages = Blueprint('error_pages', __name__)
 # 404 error page
 @error_pages.app_errorhandler(404)
 def error_404(error):
+    """Catches and reroutes HTTP 404 errors."""
+    # Delete the error variable as unused
+    del error
+    # Render 404 page
     return render_template('404.html'), 404
