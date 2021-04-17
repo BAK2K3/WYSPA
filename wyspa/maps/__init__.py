90ead6d2febb8afb6b8f527c58e5710ec515a49f
BAK2K3 <benjamin.a.kavanagh@gmail.com>
2021-04-17 16:13:59 +0000

    Add docstring for Maps module.

---
+++ wyspa/maps/__init__.py
@@ -0,0 +1,10 @@
+"""
+Maps Module
+=============
+
+This module contains the Views sub-module
+for Map functionality and routing.
+
+Sub-Module:
+    - Views (Flask Routes for Map functionality)
+"""
