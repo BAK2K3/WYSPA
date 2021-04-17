1e764d917deba14d3acb05fd3d8a6643e0aa0aa9
BAK2K3 <benjamin.a.kavanagh@gmail.com>
2021-04-17 18:03:48 +0000

    Add doc-string to main app.py

--- app.py
+++ app.py
@@ -1,3 +1,17 @@
+"""
+WYSPA - A Full Stack Anonymous Social Media Network
+===================================================
+
+WYSPA is a Full Stack Project utilising Python, NoSQL,
+JavaScript, HTML, and CSS. It aims to give voices
+to those who want to speak their mind, anonymously.
+
+See https://github.com/BAK2K3/WYSPA/blob/master/README.md
+for complete documentation.
+
+Created by Benjamin Kavanagh (BAK2K3) 2021.
+"""
+
 import os
 
 from wyspa import app
