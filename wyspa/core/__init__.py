50b22c5f54bda5a8d321336b5bb666c4a077a458
BAK2K3 <benjamin.a.kavanagh@gmail.com>
2021-04-17 16:13:28 +0000

    Update docstring.

--- wyspa/core/__init__.py
+++ wyspa/core/__init__.py
@@ -2,7 +2,8 @@
 Core Module
 =============
 
-This module contains the core Views sub-modules.
+This module contains the core Views sub-module which
+handles basic Flask routing.
 
 Sub-Module:
     - Views (Flask Routes for core functionality)
