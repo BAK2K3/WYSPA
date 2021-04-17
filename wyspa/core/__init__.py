f7346791ad56381e4d29bfffb6928fb200e4c43d
BAK2K3 <benjamin.a.kavanagh@gmail.com>
2021-04-17 15:16:58 +0000

    Add doc-string for Core Module.

---
+++ wyspa/core/__init__.py
@@ -0,0 +1,11 @@
+"""
+Core Module
+=============
+
+This module contains any sub-modules which
+do not relate to Wyspas or Users.
+
+Sub-Module:
+    - Views (Flask Routes for core functionality)
+
+"""
