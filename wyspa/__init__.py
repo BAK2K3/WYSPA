53f7f2e74af8d4f1dc424a754e69a74dd0525c5b
BAK2K3 <benjamin.a.kavanagh@gmail.com>
2021-04-18 17:00:43 +0000

    Update docstring.

--- wyspa/__init__.py
+++ wyspa/__init__.py
@@ -1,12 +1,16 @@
 """
-Wyspa Module
-============
+WYSPA - A Full Stack Anonymous Social Media Network
+===================================================
 
-This module creates an instance of the Flask application
-via the Factory, registers all blueprints, then sets up
-the Login Manager.
+WYSPA is a Full Stack Project utilising Python, NoSQL,
+JavaScript, HTML, and CSS. It aims to give voices
+to those who want to speak their mind, anonymously.
+
+See https://github.com/BAK2K3/WYSPA/blob/master/README.md
+for complete documentation.
+
+Created by Benjamin Kavanagh (BAK2K3) 2021.
 """
-
 
 from wyspa.core.views import core
 from wyspa.maps.views import maps
