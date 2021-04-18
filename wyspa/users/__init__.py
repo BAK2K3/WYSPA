9259b9bbbd2e147aa5d5881146c91476f0c92fdc
BAK2K3 <benjamin.a.kavanagh@gmail.com>
2021-04-18 17:01:52 +0000

    Add docstring to users module.

---
+++ wyspa/users/__init__.py
@@ -0,0 +1,12 @@
+"""
+Users Module
+=============
+
+This module contains the Views sub-module
+for routing relating to the Users, and the
+Classes sub-module for the User Class.
+
+Sub-Module:
+    - Views (Flask Routes for User functionality)
+    - Classes (User Class)
+"""
