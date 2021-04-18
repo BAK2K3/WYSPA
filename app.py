9805e294837d212aa4f7abaa29763735fdd908d1
BAK2K3 <benjamin.a.kavanagh@gmail.com>
2021-04-18 17:00:58 +0000

    Update docstring.

--- app.py
+++ app.py
@@ -1,15 +1,10 @@
 """
-WYSPA - A Full Stack Anonymous Social Media Network
-===================================================
+Wyspa App.py
+============
 
-WYSPA is a Full Stack Project utilising Python, NoSQL,
-JavaScript, HTML, and CSS. It aims to give voices
-to those who want to speak their mind, anonymously.
-
-See https://github.com/BAK2K3/WYSPA/blob/master/README.md
-for complete documentation.
-
-Created by Benjamin Kavanagh (BAK2K3) 2021.
+This module calls the instantiated Flask application
+from the main __init__ file, and tells the server to
+run it.
 """
 
 import os
