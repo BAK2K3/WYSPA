a1f3b184f98d2ea80856406d58d6fe7925e7102c
BAK2K3 <benjamin.a.kavanagh@gmail.com>
2021-04-18 11:29:56 +0000

    Add doc-string to Messages Module.

---
+++ wyspa/messages/__init__.py
@@ -0,0 +1,12 @@
+"""
+Messages Module
+=============
+
+This module contains the Views sub-module
+for routing relating to the Wyspas, and the
+Classes sub-module for the Wyspa Class.
+
+Sub-Module:
+    - Views (Flask Routes for Wyspa functionality)
+    - Classes (Wyspa Class)
+"""
