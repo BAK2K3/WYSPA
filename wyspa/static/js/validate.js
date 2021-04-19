3af0515b615a28341bc8308d1dff887d25498faa
BAK2K3 <benjamin.a.kavanagh@gmail.com>
2021-04-19 17:48:22 +0000

    Update register validation to include username.

--- wyspa/static/js/validate.js
+++ wyspa/static/js/validate.js
@@ -1,33 +1,59 @@
 // Assign relevant DOM elements do variables
 const passwordRegisterField = document.querySelector("#passwordRegister");
 const passwordValidateField = document.querySelector("#passwordConfirm");
-const errorMessageField = document.querySelector(".error-message");
-const registrationForm = document.querySelector(".user-registration");
+const usernameField = document.querySelector("#usernameRegister");
+const errorMessageField = document.querySelector("#errorMessageContainer");
+const registrationForm = document.querySelector("#userRegistrationForm");
+
+// Function for username error message validation
+function usernameValidate() {
+  // If the username is not valid and the field is not empty
+  if (!usernameField.checkValidity() && usernameField.value != "") {
+    usernameField.classList.remove("valid");
+    usernameField.classList.add("invalid");
+    errorMessageField.innerHTML = "Username cannot contain spaces!";
+    // If the field is empty
+  } else if (usernameField.value == "") {
+    errorMessageField.innerHTML = "";
+    // If the field is valid
+  } else {
+    errorMessageField.innerHTML = "";
+    usernameField.classList.remove("invalid");
+    usernameField.classList.add("valid");
+    passwordValidate();
+  }
+}
 
 //  Function for Initial password validation
 function passwordValidate() {
-  // Checks against Regex pattern inline HTML
-  if (!passwordRegisterField.checkValidity()) {
-    // Change the error message to explain the issue
-    document.querySelector(".error-message").innerHTML =
-      "Password must contain 6-20 characters, and one number or special character.";
+  // If username field is valid and password field is not empty
+  if (
+    usernameField.classList.contains("valid") &&
+    passwordRegisterField.value != ""
+  ) {
+    // Checks against Regex pattern inline HTML
+    if (!passwordRegisterField.checkValidity()) {
+      // Change the error message to explain the issue
+      errorMessageField.innerHTML =
+        "Password must contain 6-20 characters, and one number or special character.";
 
-    // Apply invalid class
-    if (passwordRegisterField.classList.contains("valid")) {
-      passwordRegisterField.classList.remove("valid");
-      passwordRegisterField.classList.add("invalid");
+      // Apply invalid class
+      if (passwordRegisterField.classList.contains("valid")) {
+        passwordRegisterField.classList.remove("valid");
+        passwordRegisterField.classList.add("invalid");
+      }
+    } else {
+      // Reset the error message when correct
+      errorMessageField.innerHTML = "";
+      //  Apply valid class
+      if (passwordRegisterField.classList.contains("invalid")) {
+        passwordRegisterField.classList.remove("invalid");
+        passwordRegisterField.classList.add("valid");
+      }
     }
-  } else {
-    // Reset the error message when correct
-    document.querySelector(".error-message").innerHTML = "";
-    //  Apply valid class
-    if (passwordRegisterField.classList.contains("invalid")) {
-      passwordRegisterField.classList.remove("invalid");
-      passwordRegisterField.classList.add("valid");
-    }
+    // Run the passwordCompare function to check the confirmation password
+    passwordCompare();
   }
-  // Run the passwordCompare function to check the confirmation password
-  passwordCompare();
 }
 
 //  Function for ensuring both passwords are the same
@@ -47,8 +73,6 @@ function passwordCompare() {
       // Adjust classes
       passwordValidateField.classList.remove("valid");
       passwordValidateField.classList.add("invalid");
-
-      return false;
     }
     // If the passwords match, and there is a password in the confirmation field
     else if (
@@ -58,14 +82,14 @@ function passwordCompare() {
       errorMessageField.innerHTML = "";
       passwordValidateField.classList.remove("invalid");
       passwordValidateField.classList.add("valid");
-      return true;
     }
   }
 }
 
-// Apply event listeners to both password fields
+// Apply event listeners to register fields
 passwordRegisterField.addEventListener("change", passwordValidate);
 passwordValidateField.addEventListener("change", passwordCompare);
+usernameField.addEventListener("change", usernameValidate);
 
 // Apply submit event to check whether the above fields are Valid
 registrationForm.addEventListener("submit", function (event) {
