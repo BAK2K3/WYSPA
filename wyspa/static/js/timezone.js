125fd62fd3ed7fb86a19b2c05434fe0b1817d512
BAK2K3 <benjamin.a.kavanagh@gmail.com>
2021-04-20 14:59:34 +0000

    Add source comment.

--- wyspa/static/js/timezone.js
+++ wyspa/static/js/timezone.js
@@ -1,5 +1,6 @@
 // Pushes timezone to hidden form on edit and creation pages
 const hiddenFormLogin = document.querySelector("#timezoneLogin");
 const hiddenFormRegister = document.querySelector("#timezoneRegister");
+// https://stackoverflow.com/questions/1091372/getting-the-clients-time-zone-and-offset-in-javascript
 hiddenFormLogin.value = Intl.DateTimeFormat().resolvedOptions().timeZone;
 hiddenFormRegister.value = Intl.DateTimeFormat().resolvedOptions().timeZone;
